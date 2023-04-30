dc031b2939a95653b9e67ba9c36d5af25ca50701
MenachemNatanson <menachem.natanson@gmail.com>
2023-04-30 11:27:21 +0300

    add name to index's title

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ export default function Index() {
   ];
   return (
     <div className={styles.container}>
-      <h1>Dev&apos;s Team 2023 Basic Training</h1>
+      <h1>Dev&apos;s Team 2023 Basic Training - Mor</h1>
       <h2>
         We&apos;re learning how to build a full-stack web application with Eyal
       </h2>
