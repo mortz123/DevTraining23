158dd90478b0f715addffbc44184f88695845e58
MenachemNatanson <menachem.natanson@gmail.com>
2023-05-01 09:58:44 +0300

    fix eslint issues

--- pages/day1b.jsx
+++ pages/day1b.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Image from 'next/image';
-import styles from './day1b.module.css';
 import Image from 'next/image';
+import styles from './day1b.module.css';
 
 export default function Day1b() {
   return (
